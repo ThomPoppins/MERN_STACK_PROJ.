454ee0e496ffc08f2f2d41757370b2a31aa24649
Thom Veldpaus <thompoppins@gmail.com>
2023-11-15 06:16:13 +0100

    React dropzone working well in dev file

--- frontend/src/pages/dev/ImageCrop.jsx
+++ frontend/src/pages/dev/ImageCrop.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 
 const ImageCrop = () => {
+  const [preview, setPreview] = useState('')
+
   const onDrop = useCallback((acceptedFiles) => {
     console.log('acceptedFiles: ', acceptedFiles)
 
@@ -11,6 +12,7 @@ const ImageCrop = () => {
       // File reader to read the file
       const reader = new FileReader()
 
+      reader.readAsDataURL(file)
       // Read the file as a binary string
       // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsBinaryString
       // https://developer.mozilla.org/en-US/docs/Web/API/FileReader
@@ -20,7 +22,6 @@ const ImageCrop = () => {
       // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsArrayBuffer
       // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
       // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsText
-
       // .onabort: A handler for the abort event. This event is triggered each time the reading operation is aborted.
       reader.onabort = () => console.log('file reading was aborted')
       // .onerror: A handler for the error event. This event is triggered each time the reading operation encounter an error.
@@ -28,9 +29,11 @@ const ImageCrop = () => {
       // .onload: A handler for the load event. This event is triggered each time the reading operation is successfully completed.
       reader.onload = () => {
         // Binary string of the file contents
-        const binaryStr = reader.result
+        const base64String = reader.result
 
-        console.log('binaryStr: ', binaryStr)
+        setPreview(base64String)
+
+        console.log('base64String: ', base64String)
       }
       reader.readAsArrayBuffer(file)
     }, [])
@@ -38,19 +41,31 @@ const ImageCrop = () => {
 
   const { getInputProps, getRootProps, isDragActive } = useDropzone({ onDrop })
 
+  useEffect(() => {
+    console.log('preview: ', preview)
+  }, [preview])
+
   return (
-    <div
-      {...getRootProps({
-        className: 'dropzone bg-purple-500 text-white p-4 rounded-lg mt-4 mx-4',
-      })}
-    >
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag &apos;n&apos; drop some files here, or click to select files</p>
-      )}
-    </div>
+    <>
+      <div
+        {...getRootProps({
+          className:
+            'dropzone bg-purple-500 text-white p-4 rounded-lg mt-4 mx-4',
+        })}
+      >
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>
+            Drag &apos;n&apos; drop some files here, or click to select files
+          </p>
+        )}
+      </div>
+      <div>
+        <img alt='placeholder' id='preview' src={preview} />
+      </div>
+    </>
   )
 }
 
